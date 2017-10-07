931f4916d8d6786553e22f18892e4cfbb5ce722c
qaswedxz <32535568+qaswedxz@users.noreply.github.com>
2017-10-07 16:12:48 +0700

    (head main base ) + twig

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -1,17 +1,3 @@
 exports.index = function(req,res) {
-	let content = {
-		banner:[
-		{
-			id: 1,
-			url: '//image/img-1.jpg',
-			title: 'new 1'
-		},
-		{
-			id: 2,
-			url:'//image/img-1.jpg',
-			title: 'new 2'
-		}
-		]
-	}
-	res.json(content)
+	res.render('news.twig')
 }
